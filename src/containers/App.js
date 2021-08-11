71888eb9800127eef3912ec7b763925e152a7498
Shaun Forster <shaun.s.forster@gmail.com>
2021-08-11 13:11:11 +0100

    Converted App.js to use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchField: ''
-		}
-	}
+function App() {
+	const [robots, setRobots] = useState([]);
+	const [searchField, setSearchField] = useState('');
 
-	componentDidMount() {
+	// This fetches users every time the app renders, that is
+	// when the searchField changes.
+	useEffect(() => {
 		fetch('https://jsonplaceholder.cypress.io/users')
 		.then(response => response.json())
-		.then(users => this.setState({ robots: users }));
+		.then(users => {setRobots(users)});
+	}, []);
+
+	const onSearchChange = (event) => {
+		setSearchField(event.target.value)
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchField: event.target.value })
-	}
-
-	render() {
-		const { robots, searchField } = this.state;
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchField.toLowerCase());
-		});
-		return !robots.length ?
-			<h1>Loading</h1> :
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
+	});
+	return !robots.length ?
+		<h1>Loading</h1> :
 		(
-				<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<ErrorBoundry>
-						<CardList robots = {filteredRobots} />
-						</ErrorBoundry>
-					</Scroll>
-				</div>
-			);
-	}
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={onSearchChange} />
+				<Scroll>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots} />
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
 }
 
 export default App;
